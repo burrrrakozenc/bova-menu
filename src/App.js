b4744b037d79a9f7253b45816643ca5d61dee468
burrrrakozenc <brk.ozenc@gmail.com>
2022-12-29 13:28:26 +0300

    redirection removed - menu updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-// import Menu from './Menu'
 import './App.css';
 import {Route, Routes} from "react-router";
 import IndexPage from "./pages/Index";
 import {BrowserRouter} from "react-router-dom";
-import RedirectTo from "./pages/Redirect";
 import Menu from "./Menu";
 
 function App() {
     return (
         <div className="App">
-            {/*<header className="App-header">*/}
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<IndexPage/>}/>
-                    <Route path="/menu" element={<RedirectTo/>}/>
-                    <Route path="/menu-test" element={<Menu/>}/>
+                    <Route path="/menu" element={<Menu/>}/>
                 </Routes>
             </BrowserRouter>
             {/*</header>*/}
